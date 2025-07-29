6be8107aedf8e6f8f2afd8ae40de5192629b05dd
IEyZens <roncint61@gmail.com>
2025-07-29 11:19:20 +0200

    Add string concatenation module with function implementation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to concatenate two strings and return the
+result.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the resulting string.
+    """
+    str3 = str1 + str2
+    return str3
