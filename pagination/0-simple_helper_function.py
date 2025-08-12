774d812c7edbcdf31b7065f4d800114aeb318e72
IEyZens <roncint61@gmail.com>
2025-08-12 03:42:52 +0200

    Add type hints to index_range function for improved clarity

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def index_range(page, page_size) -> tuple:
+def index_range(page: int, page_size: int) -> tuple:
     """
     Return a tuple containing the start and end indexes for pagination.
 
@@ -14,4 +14,5 @@ def index_range(page, page_size) -> tuple:
     """
     start = (page - 1) * page_size
     end = page + page_size
+
     return (start, end)
