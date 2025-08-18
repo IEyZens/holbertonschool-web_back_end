89f4ee75d2a2bea9b73700de7ad588277cccebf1
IEyZens <roncint61@gmail.com>
2025-08-18 11:13:37 +0200

    chore: initialize project with package.json and add devDependencies for testing and linting

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
