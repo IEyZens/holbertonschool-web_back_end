98d2e49e2c28d3309e42018cd28e886591c93b79
IEyZens <roncint61@gmail.com>
2025-08-18 11:26:21 +0200

    feat: update taskBlock function to define task variables within the conditional scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,7 +3,11 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    // nothing happens here
+    let task = true;
+    let task2 = false;
+
+    task;
+    task2;
   }
 
   return [task, task2];
