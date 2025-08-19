acda367020113af36d0db34644023b191b99c065
IEyZens <roncint61@gmail.com>
2025-08-19 10:21:09 +0200

    Update file for newArray

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let idx = 0;
-  for (let value of array) {
-    array[idx] = appendString + value;
-    idx += 1;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
