fc0e738f4174f4a9561d48beb396225f9960cab4
IEyZens <roncint61@gmail.com>
2025-08-19 10:17:59 +0200

    Add ';' line 2

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let idx = 0
+  let idx = 0;
   for (const value of array) {
     array[idx] = appendString + value;
     idx += 1;
