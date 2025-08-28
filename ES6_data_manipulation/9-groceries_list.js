12ba35835d5af0d80934dd08e08ae985cd954850
IEyZens <roncint61@gmail.com>
2025-08-28 16:35:22 +0200

    Add file for task 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const users = new Map();
+  users.set('Apples', 10);
+  users.set('Tomatoes', 10);
+  users.set('Pasta', 1);
+  users.set('Rice', 1);
+  users.set('Banana', 5);
+
+  return users;
+}
