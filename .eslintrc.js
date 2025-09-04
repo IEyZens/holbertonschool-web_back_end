1424ccc035a77b5a074ed3d3b75396380efbbc61
IEyZens <roncint61@gmail.com>
2025-09-04 09:21:45 +0200

    Update file for NodeJS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,37 @@
 module.exports = {
   env: {
     browser: false,
-    es2021: true,
+    es6: true,
     jest: true,
-    node: true,
   },
-  extends: ['eslint:recommended'],
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
