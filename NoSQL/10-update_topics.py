e6421b2ca67c9bc8efc75d45d38f5cc92f9996b8
IEyZens <roncint61@gmail.com>
2025-12-02 12:19:50 +0100

    Add task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""10-update_topics.py
+Update topics of a school document in a MongoDB collection.
+"""
+
+def update_topics(mongo_collection, name, topics):
+    """Update the topics of a school document in a MongoDB collection."""
+    documentUpdate = mongo_collection.update_many(name, topics)
+    return documentUpdate
