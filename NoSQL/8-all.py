60774932208219fe379c0c59d565806edd05fc91
IEyZens <roncint61@gmail.com>
2025-12-02 10:33:55 +0100

    Ajout condition if ligne 8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -5,4 +5,7 @@ if __name__ == "__main__":
     def list_all(mongo_collection):
         """List all documents in a MongoDB collection."""
         for m in mongo_collection.find():
-            print(m)
+            if not m:
+                return []
+            else:
+                print(m)
