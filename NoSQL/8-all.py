6bfe1d6d3a266c59363f3780efaec65b6d272be6
IEyZens <roncint61@gmail.com>
2025-12-02 10:36:20 +0100

    Modification condition if

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,8 +4,8 @@
 if __name__ == "__main__":
     def list_all(mongo_collection):
         """List all documents in a MongoDB collection."""
+        if not mongo_collection:
+            return []
+
         for m in mongo_collection:
-            if not m:
-                return []
-            else:
-                print(m)
+            print(m)
