24a820e9f997b10a9c24d159dc8d520a148525fd
IEyZens <roncint61@gmail.com>
2025-12-02 10:35:12 +0100

    Supprime find() et remet m

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -4,8 +4,8 @@
 if __name__ == "__main__":
     def list_all(mongo_collection):
         """List all documents in a MongoDB collection."""
-        for m in mongo_collection.find():
-            if not mongo_collection:
+        for m in mongo_collection:
+            if not m:
                 return []
             else:
                 print(m)
