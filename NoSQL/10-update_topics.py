e5bc669e60cd16eeddb355837497f80a9a5e4f6a
IEyZens <roncint61@gmail.com>
2025-12-02 12:20:56 +0100

    Add modified_count

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ Update topics of a school document in a MongoDB collection.
 def update_topics(mongo_collection, name, topics):
     """Update the topics of a school document in a MongoDB collection."""
     documentUpdate = mongo_collection.update_many(name, topics)
-    return documentUpdate
+    return documentUpdate.modified_count
