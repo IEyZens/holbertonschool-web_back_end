876a11119cabe1200d184a1cdf1b8d363232b687
IEyZens <roncint61@gmail.com>
2025-12-02 09:57:42 +0100

    Ajout documentation et shebang

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""List all documents in a MongoDB collection."""
+
 def list_all(mongo_collection):
+    """List all documents in a MongoDB collection."""
     for i in mongo_collection.find():
         print(i)
