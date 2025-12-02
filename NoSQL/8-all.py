f0c9b06a60d304ca427f87dd7c6905b8e3fddf9b
IEyZens <roncint61@gmail.com>
2025-12-02 11:23:18 +0100

    Fichier modifié

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-"""List all documents in a MongoDB collection."""
-
+"""List all documents in a MongoDB collection"""
 
 def list_all(mongo_collection):
-    """List all documents in a MongoDB collection."""
+    """List all documents in a MongoDB collection"""
     for m in mongo_collection.find():
         if not m:
             return []
         else:
             print(m)
-
 
 if __name__ == "__main__":
     list_all()
