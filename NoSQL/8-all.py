4041f2a1d4903a82e802bf89324ce2ba82b3ac67
IEyZens <roncint61@gmail.com>
2025-12-02 10:34:33 +0100

    Remplacer m par mongo_collection

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     def list_all(mongo_collection):
         """List all documents in a MongoDB collection."""
         for m in mongo_collection.find():
-            if not m:
+            if not mongo_collection:
                 return []
             else:
                 print(m)
