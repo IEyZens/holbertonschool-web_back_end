ba61e7a95a763826c7c77d54a6946cba74a2c439
IEyZens <roncint61@gmail.com>
2025-12-02 12:24:07 +0100

    Remove documentUpdate

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,7 +3,10 @@
 Update topics of a school document in a MongoDB collection.
 """
 
+
 def update_topics(mongo_collection, name, topics):
     """Update the topics of a school document in a MongoDB collection."""
-    documentUpdate = mongo_collection.update_many(name, topics)
-    return documentUpdate.modified_count
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
