8f8006a64a334c1c600711e1dfa00050a216810f
IEyZens <roncint61@gmail.com>
2025-12-02 09:58:49 +0100

    Ajout condition if

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 """List all documents in a MongoDB collection."""
 
+
 def list_all(mongo_collection):
     """List all documents in a MongoDB collection."""
     for i in mongo_collection.find():
         print(i)
+
+
+if __name__ == "__main__":
+    list_all()
