639e04c2c70e1f7b2550e82c491fe3177aef61ab
IEyZens <roncint61@gmail.com>
2025-12-08 14:13:38 +0100

    Refactor getSumOfHoods to use local variables for expansions

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,5 +1,8 @@
 export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  if (expansion1989 === undefined) expansion1989 = 89;
-  if (expansion2019 === undefined) expansion2019 = 19;
-  return initialNumber + expansion1989 + expansion2019;
+  let newExpansion1989 = expansion1989;
+  let newExpansion2019 = expansion2019;
+
+  if (newExpansion1989 === undefined) newExpansion1989 = 89;
+  if (newExpansion2019 === undefined) newExpansion2019 = 19;
+  return initialNumber + newExpansion1989 + newExpansion2019;
 }
