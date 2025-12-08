414aefcb2d97f1ee13ce813eaaba4b4947fe9265
IEyZens <roncint61@gmail.com>
2025-12-08 14:14:54 +0100

    Refactor getSumOfHoods to consolidate variable declarations

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,7 +1,5 @@
 export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  let newExpansion1989 = expansion1989;
-  let newExpansion2019 = expansion2019;
-
+  let newExpansion1989 = expansion1989; let newExpansion2019 = expansion2019;
   if (newExpansion1989 === undefined) newExpansion1989 = 89;
   if (newExpansion2019 === undefined) newExpansion2019 = 19;
   return initialNumber + newExpansion1989 + newExpansion2019;
