24f3dbfa798b4b906716eb17be9e93909d004330
IEyZens <roncint61@gmail.com>
2025-12-09 09:52:27 +0100

    Refactor concatArrays function to use spread operator and remove console log

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,1 @@
-export default function concatArrays(array1, array2, string) {
-  console.log(string = array1.concat(array2));
-}
+export default function concatArrays(array, array2, str) { return array.concat(array2, ...str); }
