9d664c0c3fd512406d79018181fe55d4de6a2501
IEyZens <roncint61@gmail.com>
2025-12-09 14:54:32 +0100

    Refactor initializeRooms function to directly return a ClassRoom instance

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,5 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const newArray = new ClassRoom(19, 20, 34);
-
-  return {newArray};
+  return ClassRoom(this.maxStudentsSize(19, 20, 34));
 }
