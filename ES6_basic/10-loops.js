294de83f6d9825138085b65e1f0677ea4f84277a
IEyZens <roncint61@gmail.com>
2025-12-09 12:24:38 +0100

    Fix appendToEachArrayValue function to correctly append string to each array value

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = array;
+  const newArray = [];
   for (const idx of newArray) {
-    const value = newArray[idx];
-    newArray[idx] = appendString + value;
+    newArray.push(appendString + array[idx]);
   }
 
-  return [appendString + [newArray[0]], appendString + [newArray[1]], appendString + [newArray[2]]];
+  return newArray;
 }
