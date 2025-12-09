7960bfe1890195d76e8f23fa3c263cb9335962cb
IEyZens <roncint61@gmail.com>
2025-12-09 15:08:57 +0100

    Fix initializeRooms function to use correct property name for maxStudentsSize

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -2,9 +2,9 @@ import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
   const threeClass = [
-    { maxStudentsSize: 19 },
-    { maxStudentsSize: 20 },
-    { maxStudentsSize: 34 }
+    { _maxStudentsSize: 19 },
+    { _maxStudentsSize: 20 },
+    { _maxStudentsSize: 34 }
   ];
 
   return threeClass;
