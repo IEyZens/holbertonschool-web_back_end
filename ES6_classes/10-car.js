e10309aab283d2f3f32ed6dab438b6cec3ebd104
IEyZens <roncint61@gmail.com>
2025-12-11 13:47:51 +0100

    Fix

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,17 +1,5 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string') {
-      throw new TypeError('It must be an string.');
-    }
-
-    if (typeof motor !== 'string') {
-      throw new TypeError('It must be an string.');
-    }
-
-    if (typeof color !== 'string') {
-      throw new TypeError('It must be an string.');
-    }
-
     this._brand = brand;
     this._motor = motor;
     this._color = color;
