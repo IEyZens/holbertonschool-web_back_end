2170089c76ff12dd9f8ccb1584cacc13ca124531
IEyZens <roncint61@gmail.com>
2025-12-11 09:28:28 +0100

    Implement HolbertonCourse class with constructor and getters/setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,1 +1,33 @@
-// Fichier non vide
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set name(name) {
+    name;
+  }
+
+  get length() {
+    return this.length;
+  }
+
+  set length(length) {
+    length;
+  }
+
+  get students() {
+    return this.students;
+  }
+
+  set students(students) {
+    students;
+  }
+}
+
+HolbertonCourse();
