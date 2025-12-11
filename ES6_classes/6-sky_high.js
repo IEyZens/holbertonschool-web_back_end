5a16a17c72f457149c60dca4db12215d749720d8
IEyZens <roncint61@gmail.com>
2025-12-11 11:29:01 +0100

    Fix

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,12 +1,11 @@
 import { Building } from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
+  constructor(floors) {
     if (typeof floors !== 'number') {
       throw new TypeError('It must be an integer.');
     }
 
-    this._sqft = sqft;
     this._floors = floors;
   }
 
