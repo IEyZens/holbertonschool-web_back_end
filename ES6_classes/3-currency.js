e81ea1766b5641efcf00c3b8d33d8210ef4dcfab
IEyZens <roncint61@gmail.com>
2025-12-11 09:38:55 +0100

    Implement Currency class with constructor, getters, setters, and display method

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw TypeError('It must be a string.')
+    } else {
+      this._code = code;
+    }
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('It must be a string.')
+    } else {
+      this._name = name;
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`
+  }
+}
+
+Currency();
