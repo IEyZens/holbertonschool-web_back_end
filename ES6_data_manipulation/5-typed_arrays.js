00ebce45dc9f68f75c9d90b7e62cd9ad7e0081f8
IEyZens <roncint61@gmail.com>
2025-12-15 12:21:44 +0100

    Fix createInt8TypedArray function to return a DataView instead of an Int8Array

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
-  int8Array[position] = value;
-  return int8Array;
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+  return view;
 }
