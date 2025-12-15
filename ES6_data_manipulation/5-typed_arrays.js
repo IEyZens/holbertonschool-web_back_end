2f670104b32e01c5c5054f618f0e75911fda112b
IEyZens <roncint61@gmail.com>
2025-12-15 12:19:43 +0100

    Fix createInt8TypedArray function to correctly return an Int8Array instead of an ArrayBuffer

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
-  let buffer = new ArrayBuffer(length);
-  buffer[position] = value;
-  return buffer;
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  int8Array[position] = value;
+  return int8Array;
 }
