d9ec9ccfaaec0e35d4dc05a006a6855b12a3aa42
IEyZens <roncint61@gmail.com>
2025-12-15 10:55:00 +0100

    Fix

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 import getListStudents from './0-get_list_students.js';
 
 export default function getListStudentIds(id) {
-  return id.map(getListStudents(id));
+  return id.map(getListStudentIds);
 }
