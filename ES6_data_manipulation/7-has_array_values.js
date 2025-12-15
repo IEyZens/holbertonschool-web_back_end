49fca397adf96a1200f4e4eb5e990486eebb708c
IEyZens <roncint61@gmail.com>
2025-12-15 12:32:51 +0100

    Fix hasValuesFromArray function to return correct boolean values for set and array comparison

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -2,8 +2,8 @@ export default function hasValuesFromArray(set, array) {
   if (array === set) {
     return true;
   } else if (array !== set) {
-    return true;
-  } else {
     return false;
+  } else {
+    return true;
   }
 }
