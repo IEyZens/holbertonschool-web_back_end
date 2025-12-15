98563c6f6eca6ff8556eeaec9f477053c85105ea
IEyZens <roncint61@gmail.com>
2025-12-15 12:45:00 +0100

    Fix hasValuesFromArray function to correctly iterate through array elements

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,9 @@
 export default function hasValuesFromArray(set, array) {
-  if (set.has(array)) {
-    return true;
-  } else {
-    return false;
+  while (array) {
+    if (set.has(element)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
