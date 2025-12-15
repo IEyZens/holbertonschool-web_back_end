ea9a2da7844641dc0daebb62d151bc714ad2c83c
IEyZens <roncint61@gmail.com>
2025-12-15 12:54:58 +0100

    Refactor cleanSet function to convert set to array and filter elements by starting string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,9 @@
-export default function cleanSet(set, startString) {
-  return set.filter(startString.join('-'));
+function cleanSet(set, startString) {
+  const array = Array.from(set);
+
+  const filtered = array
+    .filter(value => value.startsWith(startString))
+    .map(value => value.slice(startString.length));
+
+  return filtered.join('-');
 }
