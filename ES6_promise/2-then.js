4224842ad4d62ee26d8dc54bc28646aec224958b
IEyZens <roncint61@gmail.com>
2025-12-18 15:17:05 +0100

    Fix

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,18 +2,16 @@ export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     resolve({ status: 200, body: "success" });
     reject(new Error());
-  });
+  })
+    .then((handleResponse) => {
+      console.log(handleResponse);
+    })
+
+    .catch((error) => {
+      console.error(error);
+    })
+
+    .finally(() => {
+      console.log("Got a response from the API");
+    });
 }
-
-handleResponseFromAPI()
-  .then((handleResponse) => {
-    console.log(handleResponse);
-  })
-
-  .catch((error) => {
-    console.error(error);
-  })
-
-  .finally(() => {
-    console.log("Got a response from the API");
-  });
