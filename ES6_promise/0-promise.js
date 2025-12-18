b6775e6b1e0324019a61da5d259e30e73e6afea4
IEyZens <roncint61@gmail.com>
2025-12-18 09:33:32 +0100

    fix: implement Promise executor in getResponseFromAPI to handle response

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,3 +1,13 @@
 function getResponseFromAPI() {
-  return new Promise();
+  const promise = new Promise((resolve, reject) => {
+    let response = true;
+
+    if (response == true) {
+      resolve("It's true!");
+    } else {
+      reject("It's false!");
+    }
+  });
+
+  return promise;
 }
