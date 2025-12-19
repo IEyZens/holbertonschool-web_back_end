9ef43e7118d09244d863a67806b74f27e46f14dd
IEyZens <roncint61@gmail.com>
2025-12-19 11:12:46 +0100

    Fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { createUser, uploadPhoto } from "./utils.js";
 
 export default function handleProfileSignup() {
-  Promise.all([createUser, uploadPhoto]).then((value) => {
-    console.log(value);
+  Promise.all([createUser, uploadPhoto]).then(() => {
+    console.log(`${body} ${firstName} ${lastName}`);
   });
 }
