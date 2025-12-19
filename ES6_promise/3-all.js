93367705ca96138ec4363b323945e2b5f74cf81e
IEyZens <roncint61@gmail.com>
2025-12-19 11:00:06 +0100

    Fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,10 @@
 import { createUser, uploadPhoto } from "./utils.js";
 
 export default function handleProfileSignup() {
-  const p1 = Promise.resolve(createUser);
-  const p2 = Promise.resolve(uploadPhoto);
+  const p1 = Promise.resolve(uploadPhoto);
+  const p2 = Promise.resolve(createUser);
 
-  const p3 = new Promise((resolve, reject) => {
-    reject(console.log("Signup system offline"));
+  Promise.all([p1, p2]).then((value) => {
+    console.log(value);
   });
-
-  Promise.all([p1, p2, p3]).console.log(`${uploadPhoto} ${createUser}`);
 }
