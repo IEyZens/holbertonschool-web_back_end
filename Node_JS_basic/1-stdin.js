9dbb5aa7e908a7f89fde4af0b2548d6fc97798e6
IEyZens <roncint61@gmail.com>
2026-01-05 10:32:28 +0100

    Update file

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,6 +2,5 @@ console.log("Welcome to Holberton School, what is your name?");
 
 process.stdin.on("data", (data) => {
   process.stdout.write(`Your name is: ${data}`);
-  console.log("This important software is now closing");
-  process.exit();
+  process.exit(console.log("This important software is now closing"));
 });
