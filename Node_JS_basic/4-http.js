9b46a42e24844b25590ac94cfb5f4028e1c2a331
IEyZens <roncint61@gmail.com>
2026-01-06 10:04:31 +0100

    Update file

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,9 +2,10 @@ const http = require('node:http');
 
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
 
 const PORT = 1245;
-app.listen(PORT, () => {
-  console.log('Hello Holberton School!');
+app.listen(PORT, 'localhost', () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
